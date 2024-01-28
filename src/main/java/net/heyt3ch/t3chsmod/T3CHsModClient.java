01abbaee32c90b300d745eb1a01ee409aaf0dee1
Holden Sysler <hasysler@gmail.com>
2024-01-28 00:34:13 -0500

    Initial commit

---
+++ src/main/java/net/heyt3ch/t3chsmod/T3CHsModClient.java
@@ -0,0 +1,13 @@
+package net.heyt3ch.t3chsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class T3CHsModClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
