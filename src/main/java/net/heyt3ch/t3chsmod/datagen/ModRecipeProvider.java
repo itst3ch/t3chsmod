46ab97d5f65c2fa11c2d66cd69247f4768780d8c
itst3ch <hasysler@gmail.com>
2024-02-24 16:05:45 -0500

    i forgot to actually make the recipes correct whoops i'll do it later

--- src/main/java/net/heyt3ch/t3chsmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/heyt3ch/t3chsmod/datagen/ModRecipeProvider.java
@@ -2,13 +2,13 @@ package net.heyt3ch.t3chsmod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.heyt3ch.t3chsmod.item.ModdedItems;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.SmithingTransformRecipeJsonBuilder;
 import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.item.Equipment;
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.Registries;
@@ -26,11 +26,15 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         {
             if (item instanceof Equipment && EnchantmentTarget.ARMOR.isAcceptableItem(item))
             {
-                //create smithing recipe
                 String recipeId = "t3chsmod:" + item.toString().toLowerCase() + "_halite_infusion_recipe";
-                SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(ModdedItems.HALITE_UPGRADE), Ingredient.ofItems(item), Ingredient.ofItems(ModdedItems.HALITE_CRYSTAL), RecipeCategory.COMBAT, Items.ACACIA_BOAT)
-                        .criterion(FabricRecipeProvider.hasItem(ModdedItems.HALITE_CRYSTAL), FabricRecipeProvider.conditionsFromItem(ModdedItems.HALITE_CRYSTAL)).offerTo(exporter, recipeId);
-                System.out.println(item); //debug
+                SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(ModdedItems.HALITE_UPGRADE),
+                                Ingredient.ofItems(item),
+                                Ingredient.ofItems(ModdedItems.HALITE_CRYSTAL),
+                                RecipeCategory.COMBAT,
+                                item)
+                        .criterion(FabricRecipeProvider.hasItem(ModdedItems.HALITE_CRYSTAL),
+                                FabricRecipeProvider.conditionsFromItem(ModdedItems.HALITE_CRYSTAL)).
+                        offerTo(exporter, recipeId);
             }
         }
     }
