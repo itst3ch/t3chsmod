348bcc00b6916fbae18b207c7c214d489a5b2bf2
Holden Sysler <hasysler@gmail.com>
2024-02-24 00:44:33 -0500

    recipes?

--- src/main/java/net/heyt3ch/t3chsmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/heyt3ch/t3chsmod/datagen/ModRecipeProvider.java
@@ -4,13 +4,13 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.heyt3ch.t3chsmod.item.ModdedItems;
 import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.RecipeProvider;
 import net.minecraft.data.server.recipe.SmithingTransformRecipeJsonBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
     public ModRecipeProvider(FabricDataOutput output) {
@@ -22,10 +22,13 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     {
         for(Item item : Registries.ITEM)
         {
-            if (Registries.ITEM.getEntry(item).isIn(ModItemTagProvider.ARMOR))
+            if (RegistryEntry.of(item).isIn(ModItemTagProvider.ARMOR))
             {
                 //create smithing recipe
-                SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(ModdedItems.HALITE_UPGRADE), Ingredient.ofItems(item), Ingredient.ofItems(ModdedItems.HALITE_CRYSTAL), RecipeCategory.COMBAT, Items.ACACIA_BOAT);
+                String recipeId = "t3chsmod:" + item.toString().toLowerCase() + "_halite_infusion_recipe";
+                SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(ModdedItems.HALITE_UPGRADE), Ingredient.ofItems(item), Ingredient.ofItems(ModdedItems.HALITE_CRYSTAL), RecipeCategory.COMBAT, Items.ACACIA_BOAT)
+                        .offerTo(exporter, recipeId);
+                System.out.println(item); //debug
             }
         }
     }
